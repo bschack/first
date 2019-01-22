0a341fc95593c547d9c834ba841c38be1ec1d2e0
bschack <benharris1102@gmail.com>
2019-01-22 16:48:17 -0500

    python

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return 'Hello, world!'
+
+@app.route("/")
+def name():
+	headline = "Hullo"
+	while True:
+		now = datetime.datetime.now()
+		new_year = now.month == 1 and now.day == 1
+		hour = now.hour
+		minute = now.minute
+		second = now.second
+		return render_template("page1.html", new_year=new_year, hour=hour, minute=minute, second=second)
+
+
+
+@app.route("/bye")
+def bye():
+	headline = "byebye"
+	return render_template("page2.html", headline=headline)
